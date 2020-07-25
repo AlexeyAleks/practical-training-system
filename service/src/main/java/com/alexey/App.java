c87b507257c3d17ec8794553012ecd27fe3bafdd
AlexeyAleks <AlexeyAleks@mail.ru>
2020-07-25 18:39:25 +0300

    create multi module

---
+++ service/src/main/java/com/alexey/App.java
@@ -0,0 +1,13 @@
+package com.alexey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
