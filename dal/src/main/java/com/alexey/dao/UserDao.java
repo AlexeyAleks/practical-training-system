37691f909f08e871918f990652d294df223d518f
AlexeyAleks <AlexeyAleks@mail.ru>
2020-07-26 14:54:19 +0300

    modify 'dao', 'impl'

--- dal/src/main/java/com/alexey/dao/UserDao.java
+++ dal/src/main/java/com/alexey/dao/UserDao.java
@@ -2,5 +2,5 @@ package com.alexey.dao;
 
 import com.alexey.entity.User;
 
-public interface UserDaoImpl extends Dao<User> {
+public interface UserDao extends Dao<User> {
 }
---
+++ dal/src/main/java/com/alexey/dao/impl/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package com.alexey.dao.impl;
+
+import com.alexey.dao.UserDao;
+import com.alexey.entity.User;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void creat(User user) {
+
+    }
+
+    @Override
+    public User readById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
